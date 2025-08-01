aed5a18f58df5aa925f5ec3ab2e8db6df7458f6b
Melati11 <2110631250049@student.unsika.ac.id>
2025-08-01 19:14:39 +0700

    update lagi

---
+++ main.js
@@ -0,0 +1,53 @@
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+    threshold: 0.1,
+    rootMargin: "0px 0px -50px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+const divider = document.querySelector('.fade-divider');
+const appearDivid = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+
+appearDivid.observe(divider);
+
+const orders = document.querySelectorAll('.fade-order');
+const appearOrder = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+}, {
+    threshold: 0.5
+});
+
+orders.forEach(order => {
+    appearOrder.observe(order);
+});
